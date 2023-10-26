1a7ace0676afe0cc6e6eb910ae725fa294a93bf8
Bornamd <borna.djavdan@gmail.com>
2023-10-26 14:10:20 +0200

    Initialization of the R project space

---
+++ copilot test.R
@@ -0,0 +1,8 @@
+# import a toy dataset
+data(iris)
+# create a new column with the species name 
+iris$Species <- as.character(iris$Species)
+# create a new column with the species name
+iris$Species <- as.character(iris$Species)
+
+
